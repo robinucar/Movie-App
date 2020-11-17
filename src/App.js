343b1b4e984434d217b6a20b82329cad0a553c41
robinucar <mehmet.ucar@imediapro.co.uk>
2020-11-17 00:14:30 +0000

    default css updated

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+        <h1 className='title'>React Movie Search</h1>
     </div>
   );
 }
