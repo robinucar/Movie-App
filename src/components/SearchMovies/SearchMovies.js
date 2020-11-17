537a3ed2027c95488a3491bec3b9ea4e6e7d6586
robinucar <mehmet.ucar@imediapro.co.uk>
2020-11-17 15:52:23 +0000

    display movies

--- src/components/SearchMovies/SearchMovies.js
+++ src/components/SearchMovies/SearchMovies.js
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import classes from "../SearchMovies/SearchMovies.module.css";
 const SearchMovies = () => {
   const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
-
-   
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=d12aa4b67431768e314a8f590cafe2cc&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url);
-      const data = res.json();
-      console.log(data);
+      const data = await res.json();
+      console.log(data.results);
+      setMovies(data.results);
     } catch (err) {
       console.log(err);
     }
@@ -30,12 +30,36 @@ const SearchMovies = () => {
           type="text"
           name="query"
           placeholder="i.e. Jurassic Park"
-          value={query} onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit" className={classes.button}>
           Search
         </button>
       </form>
+      <div>
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card" key={movie.id}>
+              <img
+                className="card-image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + "poster"}
+              />
+              <div className="card-content">
+                <h3 className="card-title">{movie.title}</h3>
+                <p>
+                  <small>RELEASE DATE: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>RATING: {movie.vote_average}</small>
+                </p>
+                <p className="card-desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
