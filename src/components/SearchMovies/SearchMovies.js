cbbaaba6599f24391b3503ab67b6bc3c38929f57
robinucar <mehmet.ucar@imediapro.co.uk>
2020-11-17 01:23:51 +0000

    searchMovies function gets data from api

--- src/components/SearchMovies/SearchMovies.js
+++ src/components/SearchMovies/SearchMovies.js
@@ -1,9 +1,25 @@
 import React from "react";
-import classes from '../SearchMovies/SearchMovies.module.css';
+import classes from "../SearchMovies/SearchMovies.module.css";
 const SearchMovies = () => {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=d12aa4b67431768e314a8f590cafe2cc&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={searchMovies}>
         <label htmlFor="query" className={classes.label}>
           Movie Name
         </label>
