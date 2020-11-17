e5d675f1c848eb8df482c6060416b2ba5fcd1154
robinucar <mehmet.ucar@imediapro.co.uk>
2020-11-17 01:32:31 +0000

    query state

--- src/components/SearchMovies/SearchMovies.js
+++ src/components/SearchMovies/SearchMovies.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../SearchMovies/SearchMovies.module.css";
 const SearchMovies = () => {
+  const [query, setQuery] = useState("");
+
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const query = "Jurassic Park";
+   
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=d12aa4b67431768e314a8f590cafe2cc&language=en-US&query=${query}&page=1&include_adult=false`;
 
@@ -24,10 +26,11 @@ const SearchMovies = () => {
           Movie Name
         </label>
         <input
+          className={classes.input}
           type="text"
           name="query"
           placeholder="i.e. Jurassic Park"
-          className={classes.input}
+          value={query} onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit" className={classes.button}>
           Search
